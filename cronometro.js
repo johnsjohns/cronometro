f02e5aadb95d41c14b90f9ad1dadfc421f9578fd
johnsjohns <o-anao@hotmail.com>
2020-07-20 00:01:02 -0300

    Inserido logica de programação

--- cronometro.js
+++ cronometro.js
@@ -1,12 +1,38 @@
-function carregar(){
+var minuto = 0;
+var segundo = 0;
+var hora = 0;
+var contar;
+
+
+function carregar() {
     document.getElementById("start").onclick = start;
     document.getElementById("stop").onclick = pare;
 }
 
-function pare(){
-    alert("parou!");
+function pare() {
+    clearInterval(contar)
 }
 
-function start(){
-    alert("Comecou!!!!")
+function start() {
+    contar = setInterval(cronometrar, 1000);
+}
+
+
+function cronometrar() {
+    if (segundo > 59) {
+        minuto++;
+        segundo = 0;
+    }
+    document.getElementById("relogio").innerHTML = formatoData(hora) + ":" + formatoData(minuto) + ":" + formatoData(segundo);
+    segundo++;
+}
+
+function formatoData(numero) {
+    texto = "";
+    if (numero < 10) {
+        texto = "0" + numero;
+    } else {
+        texto = numero;
+    }
+    return texto;
 }
